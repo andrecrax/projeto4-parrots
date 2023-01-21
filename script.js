f2470494ddf13b305e35e5f5acabfe3fba71d33d
André Craco <49844995+andrecrax@users.noreply.github.com>
2023-01-20 21:47:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -109,14 +109,3 @@ function createCards() {
   }
   iniciarJogo();
   createCards(numberCards);
-
-/* minhaArray.sort(comparador); // Após esta linha, a minhaArray estará embaralhada
-
-// Esta função pode ficar separada do código acima, onde você preferir
-function comparador() { 
-	return Math.random() - 0.5; 
-} 
-
-
-
-*/
